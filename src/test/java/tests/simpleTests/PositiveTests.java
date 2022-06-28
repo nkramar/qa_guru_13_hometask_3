06cce0d1e832b2ca9d68590e4439c698dba44902
Kramar Nadezhda <nadezhda.kramar@telekom.com>
2022-06-28 17:31:20 +0300

    added

---
+++ src/test/java/tests/simpleTests/PositiveTests.java
@@ -0,0 +1,45 @@
+package tests.simpleTests;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@Tag("simple-tests")
+public class PositiveTests {
+
+  @Test
+  void test00() {
+    assertTrue(true);
+  }
+
+  @Test
+  void test01() {
+    assertTrue(true);
+  }
+
+  @Test
+  void test02() {
+    assertTrue(true);
+  }
+
+  @Test
+  void test03() {
+    assertTrue(true);
+  }
+
+  @Test
+  void test04() {
+    assertTrue(true);
+  }
+
+  @Test
+  void test05() {
+    assertTrue(true);
+  }
+
+  @Test
+  void test06() {
+    assertTrue(true);
+  }
+}
---
+++ src/test/java/tests/simpleTests/SkippedTests.java
@@ -0,0 +1,28 @@
+package tests.simpleTests;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@Tag("simple-tests")
+public class SkippedTests {
+  @Test
+  @Disabled
+  void test00() {
+    assertTrue(false);
+  }
+
+  @Test
+  @Disabled
+  void test01() {
+    assertTrue(false);
+  }
+
+  @Test
+  @Disabled("some reason")
+  void test02() {
+    assertTrue(false);
+  }
+}
